d613946b7c7efcd67dc0cc2b1c4a2da919cf10bd
Modman <aleksey.modman@yandex.ru>
2024-08-18 14:12:16 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, {StrictMode} from "react";
+import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 
 function App() {
     return (
-        <React.StrictMode>
-            <BrowserRouter>
-                <AppRouter />
-            </BrowserRouter>
-        </React.StrictMode>
-
+        <BrowserRouter>
+            <AppRouter />
+        </BrowserRouter>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@ export const Context = createContext(null);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Context.Provider value={{
-        user: new UserStore()
-    }}>
-        <App />
-    </Context.Provider>
+    <React.StrictMode>
+        <Context.Provider value={{
+            user: new UserStore()
+        }}>
+            <App />
+        </Context.Provider>
+    </React.StrictMode>
 );
