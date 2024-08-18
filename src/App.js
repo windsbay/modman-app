660f6e62fe38176bf68da62f3f56008f5d7108de
Modman <aleksey.modman@yandex.ru>
2024-08-18 14:25:44 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import Bottom from "./components/Bottom/Bottom";
 
 function App() {
     return (
         <BrowserRouter>
             <AppRouter />
+            <Bottom />
         </BrowserRouter>
     );
 }
